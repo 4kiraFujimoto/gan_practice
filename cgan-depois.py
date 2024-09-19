8dc19ab93909df2d2e974cc5e3082421ca0c59c8
4kiraFujimoto <74486931+4kiraFujimoto@users.noreply.github.com>
2024-09-19 16:58:30 +0900

    add gitignore

--- cgan-depois.py
+++ cgan-depois.py
@@ -9,6 +9,8 @@ import faulthandler
 import os
 import numpy as np
 import torch.nn.functional as F
+import glob
+from PIL import Image
 
 #Discriminatorのノイズのつけ方パターン２（depois）
 class Discriminator(nn.Module):
@@ -249,7 +251,7 @@ netL = LeNet().to(device) #authenticator
 optimD = optim.Adam(netD.parameters(), lr=0.0002)
 optimG = optim.Adam(netG.parameters(), lr=0.0002)
 optimL = optim.Adam(netL.parameters(), lr=0.0002)
-n_epochs = 1
+n_epochs = 100
 
 
 ##画像用
@@ -268,6 +270,22 @@ print('初期状態')
 write(netG)
 train(netD, netG, netL, optimD, optimG, optimL, n_epochs)
 
+
+# GIFアニメーションを作成
+def create_gif(in_dir, out_filename):
+    path_list = sorted(glob.glob(os.path.join(*[in_dir, '*']))) # ファイルパスをソートしてリストする
+    imgs = []                                                   # 画像をappendするための空配列を定義
+ 
+    # ファイルのフルパスからファイル名と拡張子を抽出
+    for i in range(len(path_list)):
+        img = Image.open(path_list[i])                          # 画像ファイルを1つずつ開く
+        imgs.append(img)                                        # 画像をappendで配列に格納していく
+ 
+    # appendした画像配列をGIFにする。durationで持続時間、loopでループ数を指定可能。
+    imgs[0].save(out_filename,
+                 save_all=True, append_images=imgs[1:], optimize=False, duration=100, loop=0)
+ 
+create_gif(in_dir='/home/fujimoto-a/research/ganpra/cgan_images', out_filename='/home/fujimoto-a/research/ganpra/cgan_images_gif/animation.gif') # GIFアニメーションを作成する関数を実行する
 
 
 ##test lenet
